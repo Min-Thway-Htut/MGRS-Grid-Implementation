e8ad6191c6201e17e474a3b2ae5a210a0c29ca42
Min <minthwayhtut568@gmail.com>
2025-05-05 14:23:54 +0200

    Updating App.js file

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Map from './components/map';
 import Navbar from './components/navbar';
 import MapWithMGRSOverlay from './components/mgrs';
-
+import SecondMap from './components/secondMap';
 
 function App() {
   return (
    <div>
     <Navbar />
-    <Map />
+    <SecondMap />
    </div>
   );
 }
